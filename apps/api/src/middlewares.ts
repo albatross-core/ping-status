06c5fdb21f101fd569b0e3c05cb5a38f30598e10
Johan <jboissard@gmail.com>
2025-10-11 14:08:05 +0100

    Fix static serving path to use relative path from workdir

--- apps/api/src/middlewares.ts
+++ apps/api/src/middlewares.ts
@@ -1,11 +1,8 @@
 import { serveStatic } from "hono/bun";
 import { createMiddleware } from "hono/factory";
-import { join } from "node:path";
 
 const STATIC_FILE_REGEX =
   /\.(js|css|png|jpg|jpeg|gif|svg|ico|woff|woff2|ttf|map)$/;
-
-const publicDir = join(import.meta.dir, "../public");
 
 export const serveWebApp = createMiddleware((c, next) => {
   if (c.req.path.startsWith("/api")) {
@@ -13,8 +10,8 @@ export const serveWebApp = createMiddleware((c, next) => {
   }
 
   if (c.req.path.match(STATIC_FILE_REGEX)) {
-    return serveStatic({ root: publicDir })(c, next);
+    return serveStatic({ root: "./apps/api/public" })(c, next);
   }
 
-  return serveStatic({ path: "/index.html", root: publicDir })(c, next);
+  return serveStatic({ path: "/index.html", root: "./apps/api/public" })(c, next);
 });
